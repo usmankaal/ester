b23d2de390e45e35d8e36d11dc5b99fd79d5d0c2
usmankaal <usmankaal@hotmail.com>
2017-05-13 19:24:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $.getJSON('data.json', function(data) {
 
- console.log(data);
- 
+var output = '<ul class ="searchresults">';
+ $.each(data, function(key,val) {
+    output += '<li>';
+    output += '<h2>'+ val.name + '</h2>';
+    output += '<img src="images/'+ val.shortname +' tn.jpng" alt="'+ val.name +'" />';
+    output += '<p>'+ val.bio +'</p>';
+    output += '</li>';
+ });
+ output += '</ul>';
+ $('#update').html(output);
 });
+
